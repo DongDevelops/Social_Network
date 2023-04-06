fd75b368fd7691f69a3aa23c991a82f188e06b93
John Doe <johndoe@example.com>
2023-04-06 10:33:45 +0900

    add css type in setting

--- Social_Network/settings.py
+++ Social_Network/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-
+import mimetypes
 import os
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
